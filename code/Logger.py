913f159f1c8b4967a673c3d38fee6c727f133223
mmorgan98 <mtomorgan98@gmail.com>
2025-01-31 11:34:25 -0700

    Log level implementation

--- code/Logger.py
+++ code/Logger.py
@@ -2,12 +2,17 @@ class Logger:
     def __init__(self, parser):
         self.logging_config = parser.get_logging_config()
         self.job_name = parser.get_job_name()
+        self.log_level = self.logging_config['log_level']
     
     def log_start(self):
         print(f"Starting job: {self.job_name}")
     
     def log_event(self,type, message):
-        print(f"{type} {message}")
+        if self.log_level.upper() == "INFO" and type in ["INFO", "ERROR"]:
+            print(f"{type} {message}")
+        elif self.log_level.upper() == "ERROR" and type == "ERROR":
+            print(f"{type} {message}")
+       
     
     def log_stop(self):
         print(f"Exiting job: {self.job_name}")
---
+++ code/main.py
@@ -0,0 +1,1 @@
+## Soon to be FastAPI for remote rest calls to start jobs. TBD
